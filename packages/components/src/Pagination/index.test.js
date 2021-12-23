3a264df19635953a428ca9ec7b43c1eb5b2c9efa
Matti Bar-Zeev <flashmattic@gmail.com>
2021-12-24 00:31:09 +0200

    fix: Add an empty test

---
+++ packages/components/src/Pagination/index.test.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+describe('Pagination component', () => {
+    it('should render the component', () => {
+        // TODO: Implement...
+        expect(1).toBeTruthy();
+    });
+});
