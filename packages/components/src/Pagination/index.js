53df9863ca6612ee85bdf4fdccb7f0d0810e2789
Matti Bar-Zeev <flashmattic@gmail.com>
2021-12-25 00:12:01 +0200

    feat: Use the usePagination hook from the hooks package

--- packages/components/src/Pagination/index.js
+++ packages/components/src/Pagination/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import usePagination from './use-pagination-hook';
+import {usePagination} from '@pedalboard/hooks';
 import './index.css';
 
 const Pagination = (props) => {
