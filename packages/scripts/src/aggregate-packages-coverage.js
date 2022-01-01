3186f94a88a1b687d0c42728e3122a4599c8ff54
Matti Bar-Zeev <flashmattic@gmail.com>
2022-01-02 00:47:54 +0200

    fix: Make the aggregated report work in parallel

--- packages/scripts/src/aggregate-packages-coverage.js
+++ packages/scripts/src/aggregate-packages-coverage.js
@@ -7,18 +7,20 @@
 
 const fs = require('fs');
 const path = require('path');
-const {execSync} = require('child_process');
+const {execSync, spawn} = require('child_process');
 
 const REPORTS_DIR_NAME = '.nyc_output';
 const PACKAGES_DIR_NAME = 'packages';
-const PACKAGE_PATH = path.resolve(process.cwd(), PACKAGES_DIR_NAME);
+const PACKAGES_PATH = path.resolve(process.cwd(), PACKAGES_DIR_NAME);
 const REPORTS_DIR_PATH = path.resolve(process.cwd(), REPORTS_DIR_NAME);
 const BLUE = '\x1b[34m%s\x1b[0m';
+const GREEN = '\x1b[32m%s\x1b[0m';
 
 // go over all the packages and produce a coverage report
-function aggregateReports() {
+async function aggregateReports() {
     createTempDir();
-    generateReports();
+    await generateReports();
+    console.log(GREEN, `Done.`);
 }
 
 /**
@@ -34,38 +36,49 @@ function createTempDir() {
 /**
  * Generate a report for each package and copies it to the temp reports dir
  */
-function generateReports() {
-    fs.readdir(PACKAGE_PATH, (err, items) => {
-        if (err) console.log(err);
-        else {
-            items.forEach((item) => {
-                const itemPath = path.resolve(PACKAGE_PATH, item);
-                fs.stat(itemPath, (error, stats) => {
-                    if (error) {
-                        console.error(error);
-                    }
-                    // if that item is a directory
-                    if (stats.isDirectory()) {
-                        // Attempt to launch the coverage command
-                        try {
-                            console.log(BLUE, `Generating report for the ${item} package...`);
-                            execSync('yarn test --coverage --silent', {cwd: itemPath, stdio: 'inherit'});
-                            // Copy the generated report to the reports dir
-                            const targetFilePath = path.resolve(itemPath, 'coverage', 'coverage-final.json');
-                            // check if the report file exists
-                            if (fs.existsSync(targetFilePath)) {
-                                console.log(BLUE, `Copying the coverage report...`);
-                                const destFilePath = path.resolve(REPORTS_DIR_PATH, `${item}.json`);
-                                fs.copyFileSync(targetFilePath, destFilePath);
-                            }
-                        } catch (error) {
-                            console.error('Failed to generate reports', error);
+async function generateReports() {
+    const promises = [];
+    const packages = await fs.promises.readdir(PACKAGES_PATH);
+
+    for (const packageName of packages) {
+        const promise = generateReportForPackage(packageName);
+        promises.push(promise);
+    }
+
+    return Promise.all(promises);
+}
+
+async function generateReportForPackage(packageName) {
+    const packagePath = path.resolve(PACKAGES_PATH, packageName);
+    const stat = await fs.promises.stat(packagePath);
+    if (stat.isDirectory()) {
+        try {
+            return new Promise((resolve, reject) => {
+                // Call the coverage command
+                console.log(BLUE, `Generating report for the ${packageName} package...`);
+                const process = spawn('yarn', ['test', '--coverage', '--silent'], {
+                    cwd: packagePath,
+                    stdio: 'inherit',
+                });
+
+                process.on('exit', (code) => {
+                    if (code !== 0) {
+                        reject(data);
+                    } else {
+                        // Copy the generated report to the reports dir
+                        const targetFilePath = path.resolve(packagePath, 'coverage', 'coverage-final.json');
+                        if (fs.existsSync(targetFilePath)) {
+                            const destFilePath = path.resolve(REPORTS_DIR_PATH, `${packageName}.json`);
+                            fs.copyFileSync(targetFilePath, destFilePath);
                         }
+                        resolve(packageName);
                     }
                 });
             });
+        } catch (error) {
+            console.error('Failed to generate reports', error);
         }
-    });
+    }
 }
 
 aggregateReports();
