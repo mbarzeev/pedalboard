4d779cf3818fa77d0c9bab9c5880e70b36e1ebb9
Matti Bar-Zeev <flashmattic@gmail.com>
2022-01-08 20:48:45 +0200

    chore: Fix typo

--- packages/scripts/bin/pedalboard-scripts.js
+++ packages/scripts/bin/pedalboard-scripts.js
@@ -9,12 +9,12 @@
 
 const {fork} = require('child_process');
 const path = require('path');
-const AGGREGATE_PACKAGES_COVERAGE_COMMAD = 'aggregatePackagesCoverage';
+const AGGREGATE_PACKAGES_COVERAGE_COMMAND = 'aggregatePackagesCoverage';
 
 const args = process.argv.slice(2);
 const command = args[0];
 
-if (command === AGGREGATE_PACKAGES_COVERAGE_COMMAD) {
+if (command === AGGREGATE_PACKAGES_COVERAGE_COMMAND) {
     const scriptPath = path.resolve(__dirname, '../src/aggregate-packages-coverage.js');
     fork(scriptPath);
 }
