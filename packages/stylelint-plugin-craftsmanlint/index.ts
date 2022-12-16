27a1e5e0a371be5a1b31ccbdc692382951f419a3
Matti Bar-Zeev <flashmattic@gmail.com>
2022-12-16 16:08:11 +0200

    chore: Change some comments

--- packages/stylelint-plugin-craftsmanlint/index.ts
+++ packages/stylelint-plugin-craftsmanlint/index.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021-present, Matti Bar-Zeev.
+ * Copyright (c) 2022-present, Matti Bar-Zeev.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
--- packages/stylelint-plugin-craftsmanlint/src/rules/index.ts
+++ packages/stylelint-plugin-craftsmanlint/src/rules/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2022-present, Matti Bar-Zeev.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import propsInFiles from './props-in-files';
 
 const rules: Record<string, any> = {
