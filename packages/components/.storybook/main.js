2356b30f65ea7801b14478c39f78ab8073fa5a00
Matti Bar-Zeev <flashmattic@gmail.com>
2024-02-01 22:42:08 +0200

    chore: Fix Chromatic build

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -2,4 +2,7 @@ module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-sass-postcss'],
     framework: '@storybook/react',
+    core: {
+        builder: 'webpack5',
+    },
 };
