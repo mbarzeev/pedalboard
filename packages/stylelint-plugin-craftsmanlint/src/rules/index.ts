b60813f5fb6e5beb79bdfe3d887d5ea9a3a28887
Noa Bentu <noa.bentolila@duda.co>
2025-12-01 16:49:06 +0200

    feat: add style rule type no-hardcoded-values

--- packages/stylelint-plugin-craftsmanlint/src/rules/index.ts
+++ packages/stylelint-plugin-craftsmanlint/src/rules/index.ts
@@ -6,9 +6,11 @@
  */
 
 import propsInFiles from './props-in-files';
+import noHardcodedValues from './no-hardcoded-values';
 
 const rules: Record<string, any> = {
     'props-in-files': propsInFiles,
+    'no-hardcoded-values': noHardcodedValues,
 };
 
 export default rules;
